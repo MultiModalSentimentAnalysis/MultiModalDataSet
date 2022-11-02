962ab54747b5172141bbaab1cec61efafd05f10d
fatemeh tohidian <thdftm@gmail.com>
2022-11-02 16:04:18 +0330

    Fix texts file name and remove frame id from subtitle lines in label studio input file

--- generate_label_studio_input.py
+++ generate_label_studio_input.py
@@ -10,8 +10,8 @@ for i, movie_path in enumerate(movie_paths):
         f"--------------------- {i+1}/{len(movie_paths)}------------------------------------"
     )
     clips = glob(movie_path+'clips/*.mkv')
-    with open(movie_path+'text.txt') as subtitel_file:
-        subtitels =  [line.rstrip() for line in subtitel_file]
+    with open(movie_path+'texts.txt') as subtitel_file:
+        subtitels =  [line.rstrip().split('-',1)[1] for line in subtitel_file]
     if len(subtitels) == len(clips):
         input_dict = {'video' : clips, 'text': subtitels}
         input_df = pd.DataFrame(input_dict)
