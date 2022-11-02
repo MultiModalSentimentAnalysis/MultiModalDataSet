67123084f9ba1100d2d99f32cc9e0e4207e56ede
Amin Kashiri <kashiri.amin@gmail.com>
2022-11-02 21:06:30 +0330

    Add dynamic label studio project creation

---
+++ label-studio/create_project.py
@@ -0,0 +1,31 @@
+import sys
+import csv
+import json
+from pathlib import Path
+sys.path.append(str(Path(".").resolve()))
+from settings import LABEL_STUDIO_URL, API_KEY, LABELSTUDIO_DIR
+
+# Import the SDK and the client module
+from label_studio_sdk import Client
+
+# Connect to the Label Studio API and check the connection
+ls = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
+ls.check_connection()
+
+template = ''.join(open(LABELSTUDIO_DIR / "template.xml").readlines())
+
+project = ls.start_project(
+    title='Test Project 3',
+    label_config=template
+)
+
+json_array = []
+with open("label-studio/input.csv", newline='\n') as csvf:
+    csvReader = csv.DictReader(csvf) 
+    for row in csvReader: 
+        json_array.append(row)
+
+json_string = json.dumps(json_array, indent=4)
+print(json_string)
+
+project.import_tasks(json_array)
