d70c9bb473f97096f6d9944fffc4fb0e1ed46b45
fatemeh tohidian <thdftm@gmail.com>
2022-11-02 15:56:19 +0330

    Add label studio input generator script

---
+++ generate_label_studio_input.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+from glob import glob
+from settings import DATA_DIR
+
+movie_paths = glob((str(DATA_DIR) +'/*/' ))
+
+for i, movie_path in enumerate(movie_paths):
+    print(
+        f"--------------------- {i+1}/{len(movie_paths)}------------------------------------"
+    )
+    clips = glob(movie_path+'clips/*.mkv')
+    with open(movie_path+'text.txt') as subtitel_file:
+        subtitels =  [line.rstrip() for line in subtitel_file]
+    if len(subtitels) == len(clips):
+        input_dict = {'video' : clips, 'text': subtitels}
+        input_df = pd.DataFrame(input_dict)
+        input_df.to_csv(movie_path+'input.csv',index = False)
+    else :
+        print('count of clips are not equal to count of subtitels for movie '+ str(movie_path))
