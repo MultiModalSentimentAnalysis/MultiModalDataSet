0c007c485028c04eaeeb291bcdcf08a0f5b8dcf3
Amin Kashiri <kashiri.amin@gmail.com>
2022-11-02 17:23:10 +0330

    Add movie_id and clip_id to label-studio input generator

--- generate_label_studio_input.py
+++ generate_label_studio_input.py
@@ -1,31 +1,45 @@
-import os
 import pandas as pd
+from pathlib import Path
 from glob import glob
-from settings import DATA_DIR
+from settings import DATA_DIR, LABELSTUDIO_DIR
 
-def save_input_file(clips, subtitles, path):
-    input_dict = {'video' : clips, 'text': subtitles}
+
+def save_input_file(movie_id, clips, subtitles, MOVIE_BASE_DIR):
+    clip_ids, clip_paths = zip(*clips)
+    input_dict = {"video": clip_paths, "text": subtitles, "clip_id": clip_ids}
     input_df = pd.DataFrame(input_dict)
-    input_df.to_csv(path+'input.csv',index = False)
+    input_df["movie_id"] = movie_id
+    input_df.to_csv(MOVIE_BASE_DIR / "input.csv", index=False)
+    return input_df
 
-movie_paths = glob((str(DATA_DIR) +'/*/' ))
 
-generate_complete_input_file  = True
-total_subtitles = []
-total_clips = []
-for i, movie_path in enumerate(movie_paths):
+movie_paths = glob((str(DATA_DIR) + "/*/"))
+
+generate_complete_input_file = True
+movie_dfs = []
+for i, MOVIE_BASE_DIR in enumerate(movie_paths):
     print(
         f"--------------------- {i+1}/{len(movie_paths)}------------------------------------"
     )
-    clips = glob(movie_path+'clips/*.mkv')
-    with open(movie_path+'texts.txt') as subtitel_file:
-        subtitles =  [line.rstrip().split('-',1)[1] for line in subtitel_file]
+    MOVIE_BASE_DIR = Path(MOVIE_BASE_DIR)
+    movie_id = MOVIE_BASE_DIR.name
+    CLIPS_DIR = MOVIE_BASE_DIR / "clips"
+    clips = glob(str(CLIPS_DIR / "*.mkv"))
+    clips = list(
+        map(lambda x: (int(x.rsplit("/")[-1].rsplit(".")[0].split("_")[1]), x), clips)
+    )
+    clips = sorted(clips)
+    with open(MOVIE_BASE_DIR / "texts.txt") as subtitel_file:
+        subtitles = [line.rstrip().split("-", 1)[1] for line in subtitel_file]
     if len(subtitles) == len(clips):
-        save_input_file(clips, subtitles, movie_path)
-        total_subtitles.extend(subtitles)
-        total_clips.extend(clips)
-    else :
-        print('count of clips are not equal to count of subtitels for movie '+ str(movie_path))
+        movie_df = save_input_file(movie_id, clips, subtitles, MOVIE_BASE_DIR)
+        movie_dfs.append(movie_df)
+    else:
+        print(
+            "count of clips are not equal to count of subtitels for movie "
+            + str(MOVIE_BASE_DIR)
+        )
 
-if generate_complete_input_file :
-    save_input_file(total_clips, total_subtitles, (str(DATA_DIR)+'/'))
+if generate_complete_input_file:
+    df = pd.concat(movie_dfs, ignore_index=True)
+    df.to_csv(LABELSTUDIO_DIR / "input.csv", index=False)
--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(".")
 DATA_DIR = BASE_DIR / "dataset"
 VIDEO_DIR = BASE_DIR / "videos"
+LABELSTUDIO_DIR = BASE_DIR / "label-studio"
 
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
